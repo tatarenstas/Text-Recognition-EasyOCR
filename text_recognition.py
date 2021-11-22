16f41443c77bee6aa2fd860415bce599d7ccfa33
Stanislav Tataren <79329397+tatarenstas@users.noreply.github.com>
2021-11-22 16:27:48 +0200

    Create text_recognition.py

---
+++ text_recognition.py
@@ -0,0 +1,17 @@
+import easyocr
+
+def text_recognition(file_path, text_file_name="result.txt"):
+    reader = easyocr.Reader(["en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+    
+    with open(text_file_name, "w") as file:
+        for line in result:
+            file.write(f"{line}\n\n")
+    
+    return f"Result wrote into {text_file_name}"
+
+def main():
+    file_path = input("Enter a file path: ")
+    print(text_recognition(file_path=file_path))
+if __name__ == "__main__":
+    main()
